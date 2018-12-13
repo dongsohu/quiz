62b42b85436f5665f5f4e573ee745d8cf1cdeb28
Danielle Ongsohu <ongsohu.d@gmail.com>
2018-12-13 23:11:21 +0000

    Day 1

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
-        var techPlacement = "TBD";
+        var typeOfFood = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
+        var q4Result = $("#question4").val();
         var totalScore;
     });
 
