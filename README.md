c3fc6b225ffee919a616fe52e9a138f75fa76fca
Danielle Ongsohu <ongsohu.d@gmail.com>
2019-01-10 23:02:24 +0000

    DAY 3

--- README.md
+++ README.md
@@ -23,20 +23,20 @@ Make sure to check off your items as you complete them. [ ] => [x]
 
 ## Day 2
 ### JS
-- [ ] Use your totalScore variable to store the sum of the result of each function when the user clicks the button.
-- [ ] Write a function that congratulates the user, by name, on their placement and displays the results on the webpage. For now their placement will be "TBD". 
+- [x] Use your totalScore variable to store the sum of the result of each function when the user clicks the button.
+- [x] Write a function that congratulates the user, by name, on their placement and displays the results on the webpage. For now their placement will be "TBD". 
 ### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 
 ## Day 3
 ### JS
-- [ ] Using the function you created to display the results and conditional statements, write a function that places the user to a tech company based on their total score.
+- [x] Using the function you created to display the results and conditional statements, write a function that places the user to a tech company based on their total score.
 ### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 ## Extensions
-- [ ] Update the HTML and CSS to reflect your own style.
-- [ ] Change the theme of the quiz.
+- [x] Update the HTML and CSS to reflect your own style.
+- [x] Change the theme of the quiz.
 - [ ] Add more questions to your quiz.
 - [ ] Update the function that displays the result so it displays an image in addition to the user-specific placement details.
--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@ $(document).ready(function() {
         var q3Result = $("#question3").val();
         var q4Result = $("#question4").val();
         var totalScore;
-        function Q1(num){
-        var question1Total;
         
-        if("Beach"){
+    function Q1(num){
+        var question1Total;
+        if(num ==="Beach"){
             return question1Total = 1;
-        }else if("City"){
+        }else if(num==="City"){
             return question1Total = 2;
-        }else if("Countryside"){
+        }else if(num === "Countryside"){
             return question1Total = 3;
         }else{
             return question1Total = 4;
@@ -25,11 +25,11 @@ $(document).ready(function() {
          }
     function Q2(num){
         var question2Total;
-        if("Winter"){
+        if(num === "Winter"){
             return question2Total = 4;
-        }else if("Spring"){
+        }else if(num === "Spring"){
             return question2Total = 3
-        }else if("Summer"){
+        }else if(num === "Summer"){
             return question2Total = 2;
         }else{
             return question2Total = 1;
@@ -37,11 +37,11 @@ $(document).ready(function() {
     }
     function Q3(num){
         var question3Total;
-        if("Tokyo, Japan"){
+        if(num === "Tokyo, Japan"){
             return question3Total = 3;
-        }else if("New York, United States"){
+        }else if(num === "New York, United States"){
             return question3Total = 1;
-        }else if("Sydney, Australia"){
+        }else if(num === "Sydney, Australia"){
             return question3Total = 2;
         }else{
             return question3Total = 4;
@@ -49,30 +49,31 @@ $(document).ready(function() {
     }
     function Q4(num){
         var question4Total;
-        if("Coco-Cola"){
+        if(num === "Coca-Cola"){
             return question4Total = 2;
-        }else if("Water"){
+        }else if(num === "Water"){
             return question4Total = 3;
-        }else if("Ice-tea"){
+        }else if(num === "Ice-tea"){
             return question4Total = 1;
         }else{
             return question4Total = 4;
         }
     }
     function finale(q1R, q2R, q3R, q4R){
-        var questionsTotal = Q1(q1Result) + Q2(q2Result) + Q3(q3Result) + Q4(q4Result);
+       var questionsTotal = Q1(q1Result) + Q2(q2Result) + Q3(q3Result) + Q4(q4Result);
+       alert(questionsTotal);
         if(questionsTotal === 16){
             $('#answer').text("You are Sushi");
-        }else if(questionsTotal < 15 && questionsTotal >11){
+        }else if(questionsTotal <= 15 && questionsTotal >=11){
             $("#answer").text("You are Burger");
-        }else if(questionsTotal < 10 && questionsTotal >6){
+        }else if(questionsTotal <= 10 && questionsTotal >=6){
             $("#answer").text("You are Fries");
         }else{
             $("#answer").text("You are Mac & Cheese");
         }
-        
+        return questionsTotal;
     }
-    
+  finale(q1Result, q2Result, q3Result, q4Result);
 });
 
     });
